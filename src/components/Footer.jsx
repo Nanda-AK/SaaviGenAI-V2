191f96ba86b7c9c28555b34c22f9a3021c3f5ed9
Asur-Daanav <542akhil@gmail.com>
2025-11-17 00:05:57 +0530

    fix

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,7 @@
 // src/components/Footer.jsx
 import React from "react";
 import { Link } from "react-router-dom";
-import logo from "../assets/logo.png";
+import logo from "../assets/logo.png"
 
 export default function Footer() {
   const currentYear = new Date().getFullYear();
